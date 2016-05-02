cdf5271db033bdfd89ad20ce31f74b538370f332
Hauke Zühl <hauke.zuehl@celexongroup.com>
2016-05-02 11:58:52 +0200

    Clean up

--- include/Telegram/exec.h
+++
@@ -1,8 +0,0 @@
-#ifndef __EXEC_H__
-#define __EXEC_H__
-
-#include <string>
-
-std::string exec(const char*);
-
-#endif
--- include/Telegram/explode.h
+++
@@ -1,9 +0,0 @@
-#ifndef __explode_h__
-#define __explode_h__
-
-#include <string>
-#include <vector>
-
-std::vector<std::string> explode( const std::string&, const std::string &);
-
-#endif
