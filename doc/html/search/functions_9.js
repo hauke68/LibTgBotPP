d6c976877955fca81691e9c2953907b1eb34a5df
Hauke Zühl <hzuehl@phone-talk.net>
2017-06-07 20:38:03 +0200

    Updated docs

--- doc/html/search/functions_9.js
+++ doc/html/search/functions_9.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['main',['main',['../example01_8cc.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'example01.cc']]],
+  ['main',['main',['../example01_8cc.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'main(int argc, char **argv):&#160;example01.cc'],['../example02_8cc.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'main(int argc, char **argv):&#160;example02.cc']]],
   ['message',['Message',['../classTelegram_1_1Message.html#a467585156f9036194509f7f969d38ee7',1,'Telegram::Message::Message(void)'],['../classTelegram_1_1Message.html#a05293cd3884ade64ae2ae9e523abafda',1,'Telegram::Message::Message(Json::Value)'],['../classTelegram_1_1Message.html#a34c66ab90402aaa8ed7cd49939112df8',1,'Telegram::Message::Message(std::string)']]],
   ['messageentity',['MessageEntity',['../classTelegram_1_1MessageEntity.html#a11eea544ae55f7ac805d4071b346d3b9',1,'Telegram::MessageEntity']]]
 ];
